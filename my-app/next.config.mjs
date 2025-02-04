2823e784773044a7a89a043619853cd67d182358
umesh2407 <choudharyumesh986@gmail.com>
2025-02-04 13:23:36 +0530

    cors solved

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,15 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "res.cloudinary.com",
-          pathname: "/dxzidv0lk/image/upload/**", // Match specific paths
-        },
-      ],
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/dxzidv0lk/image/upload/**", // Match specific paths
+      },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+        ],
+      },
+    ];
+  },
+};
+
+export default nextConfig;
